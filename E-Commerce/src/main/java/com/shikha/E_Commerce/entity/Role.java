c397b6b9d43ec18eefa41442692b2560719be435
Shreyansh Bhardwaj <shreyansh272002@gmail.com>
2026-02-12 15:00:02 +0530

    role class created

---
+++ E-Commerce/src/main/java/com/shikha/E_Commerce/entity/Role.java
@@ -0,0 +1,26 @@
+package com.shikha.E_Commerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String name;
+
+
+}
